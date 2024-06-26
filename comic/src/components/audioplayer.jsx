041304b9b6b46c9b1460abf6688b4584a25502b9
Felipe Murillo <felmurillo@estudiantec.cr>
2024-06-25 20:18:58 -0600

    dasda

--- comic/src/components/audioplayer.jsx
+++ comic/src/components/audioplayer.jsx
@@ -1,32 +1,37 @@
-// src/components/AudioPlayer.jsx
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import '../App.css';
+import { FaPlay, FaPause } from 'react-icons/fa';
 
 const AudioPlayer = ({ src, autoPlay }) => {
   const audioRef = useRef(null);
+  const [pausado, setPausado] = useState(true);
 
   useEffect(() => {
     if (autoPlay && audioRef.current) {
       audioRef.current.currentTime = 0;  // Reinicia el audio
       audioRef.current.play();
+      setPausado(false);
     }
   }, [autoPlay, src]);
 
-  const handlePlay = () => {
-    audioRef.current.play();
+  const handlePlayPause = () => {
+    if (pausado) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+    setPausado(!pausado);
   };
 
-  const handlePause = () => {
-    audioRef.current.pause();
-  };
   return (
     <div>
-      <audio ref={audioRef} src={src}  />
-    <div className='botones'>
-    <button onClick={handlePlay}>Play</button>
-    <button onClick={handlePause}>Pause</button>
-    </div>
+      <audio ref={audioRef} src={src} />
+      <div className='botones'>
+        <button onClick={handlePlayPause}>
+          {pausado ? <FaPlay /> : <FaPause />}
+        </button>
+      </div>
     </div>
   );
 };
